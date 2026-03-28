a22b67d3dc3b38c1b309a0b3c83e12b7d3f57e65
unknown <muktinathrajbanshi786@gmail.com>
2026-03-28 19:47:37 +0545

    la hai la yestai ho k garne

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ const products = [
   { id:3, name:"Bag", price:40, img:"https://via.placeholder.com/200" },
   { id:4, name:"Headphone", price:60, img:"https://via.placeholder.com/200" }
 ];
+
+let cart = [];
+
+function displayProducts(){
+  const container = document.getElementById("products");
+  container.innerHTML = "";
+
+  products.forEach(p => {
+    container.innerHTML += `
+      <div class="card">
+        <img src="${p.img}">
+        <h3>${p.name}</h3>
+        <p>$${p.price}</p>
+        <button onclick="addToCart(${p.id})">Add to Cart</button>
+      </div>
+    `;
+  });
+}
