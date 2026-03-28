5465ffc75d260a14dceb124175d7cc8835d45e23
unknown <muktinathrajbanshi786@gmail.com>
2026-03-28 20:46:10 +0545

    aja lai yati la

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const products = [
-  { id:1, name:"Shoes", price:50, img:"https://via.placeholder.com/200" },
-  { id:2, name:"Watch", price:80, img:"https://via.placeholder.com/200" },
-  { id:3, name:"Bag", price:40, img:"https://via.placeholder.com/200" },
+  { id:1, name:"Shoes", price:50, img:"./images/shooes.png" },
+  { id:2, name:"Watch", price:80, img:"./images/watch.png" },
+  { id:3, name:"Bag", price:40, img:"./images/bag.png" },
   { id:4, name:"Headphone", price:60, img:"https://via.placeholder.com/200" }
 ];
 
