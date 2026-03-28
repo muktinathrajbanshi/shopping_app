8104659f0f2b01fa9f888d8937fc7b5861020f22
unknown <muktinathrajbanshi786@gmail.com>
2026-03-28 19:46:54 +0545

    logic part started

---
+++ index.js
@@ -0,0 +1,6 @@
+const products = [
+  { id:1, name:"Shoes", price:50, img:"https://via.placeholder.com/200" },
+  { id:2, name:"Watch", price:80, img:"https://via.placeholder.com/200" },
+  { id:3, name:"Bag", price:40, img:"https://via.placeholder.com/200" },
+  { id:4, name:"Headphone", price:60, img:"https://via.placeholder.com/200" }
+];
