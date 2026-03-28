7194cb26d6e9db0932b7194771daaa5145558243
unknown <muktinathrajbanshi786@gmail.com>
2026-03-28 19:48:19 +0545

    danger mane khatra

--- index.js
+++ index.js
@@ -22,3 +22,29 @@ function displayProducts(){
     `;
   });
 }
+
+function addToCart(id){
+  const product = products.find(p => p.id === id);
+  cart.push(product);
+  updateCart();
+}
+
+function updateCart(){
+  const cartItems = document.getElementById("cartItems");
+  const totalDiv = document.getElementById("total");
+
+  cartItems.innerHTML = "";
+  let total = 0;
+
+  cart.forEach((item, index) => {
+    total += item.price;
+    cartItems.innerHTML += `
+      <div class="cart-item">
+        <span>${item.name}</span>
+        <button onclick="removeItem(${index})">❌</button>
+      </div>
+    `;
+  });
+
+  totalDiv.innerText = "Total: $" + total;
+}
