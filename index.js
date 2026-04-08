09021bb89a66504fe131d4b87f4ec214a75fc29a
unknown <muktinathrajbanshi786@gmail.com>
2026-04-08 19:26:54 +0545

    let's update logic part also

--- index.js
+++ index.js
@@ -1,78 +1,8 @@
 const products = [
-  { 
-    id:1, 
-    name:"Shoes", 
-    price:50, 
-    img:"./images/shooes.png" 
-  },
-  { 
-    id:2, 
-    name:"Watch", 
-    price:80, 
-    img:"./images/watch.png" 
-  },
-  { 
-    id:3, 
-    name:"Bag", 
-    price:40, 
-    img:"./images/bag.png" 
-  },
-  { 
-    id:4, 
-    name:"Headphone", 
-    price:60, 
-    img:"./images/headphone.png" 
-  }
+  { id: 1, name: "Sneakers", price: 50, img: "https://cdn-icons-png.flaticon.com/512/2589/2589903.png" },
+  { id: 2, name: "Smart Watch", price: 80, img: "https://cdn-icons-png.flaticon.com/512/747/747376.png" },
+  { id: 3, name: "Backpack", price: 40, img: "https://cdn-icons-png.flaticon.com/512/3081/3081559.png" },
+  { id: 4, name: "Headphones", price: 60, img: "https://cdn-icons-png.flaticon.com/512/727/727245.png" }
 ];
 
 let cart = [];
-
-function displayProducts(){
-  const container = document.getElementById("products");
-  container.innerHTML = "";
-
-  products.forEach(p => {
-    container.innerHTML += `
-      <div class="card">
-        <img src="${p.img}">
-        <h3>${p.name}</h3>
-        <p>$${p.price}</p>
-        <button onclick="addToCart(${p.id})">Add to Cart</button>
-      </div>
-    `;
-  });
-}
-
-function addToCart(id){
-  const product = products.find(p => p.id === id);
-  cart.push(product);
-  updateCart();
-}
-
-function updateCart(){
-  const cartItems = document.getElementById("cartItems");
-  const totalDiv = document.getElementById("total");
-
-  cartItems.innerHTML = "";
-  let total = 0;
-
-  cart.forEach((item, index) => {
-    total += item.price;
-    cartItems.innerHTML += `
-      <div class="cart-item">
-        <span>${item.name}</span>
-        <button onclick="removeItem(${index})">❌</button>
-      </div>
-    `;
-  });
-
-  totalDiv.innerText = "Total: $" + total;
-}
-
-
-function removeItem(index){
-  cart.splice(index,1);
-  updateCart();
-}
-
-displayProducts();
